64c1299ee1e378ffc0a8934f1b5dbea47f8061c5
javier <javierlopera988@gmail.com>
2020-03-17 20:58:01 +0100

    Primera Subida

---
+++ js/Alumno.js
@@ -0,0 +1,37 @@
+
+// Contructor usado para crear los usuarios
+
+function Usuario(nombre, apellido1, apellido2, fecha) {
+    this.nombre = nombre;
+    this.apellido1 = apellido1;
+    this.apellido2 = apellido2;
+    this.fecha = fecha;
+}
+
+let inicio = () => {
+    let nombre = opener.document.getElementById("nombre").value;
+    let apellido1 = opener.document.getElementById("apellido1").value;
+    let apellido2 = opener.document.getElementById("apellido2").value;
+    let fecha = opener.document.getElementById("fecha").value;
+
+    // Creo un nuevo usuario:
+
+    alumno = new Usuario(nombre, apellido1, apellido2, fecha);
+
+    alumno.mostrarUsuario();
+
+}
+
+ Usuario.prototype.mostrarUsuario = function (){
+
+    container = document.getElementById("container");
+    container.innerHTML = `
+                    <h1>Javier Lopera Jiménez</h1>
+                    <p>Nombre del usuario: `+ alumno.nombre + `</p>
+                    <p>Apellido 1: `+ alumno.apellido1 + `</p>
+                    <p>Apellido 2: `+ alumno.apellido2 + `</p>
+                    <p>Fecha de Nacimiento: `+ alumno.fecha + `</p>
+        `;
+}
+
+document.addEventListener("DOMContentLoaded", inicio);
---
+++ js/main.js
@@ -0,0 +1,119 @@
+
+    // let preferenciasVentana = "left=400px,top=300px,width=300px,height=200px";
+
+
+     comprobarFechaNacimiento = function (fecha) {
+
+        // Expresion utilizada para validar la fecha de nacimiento
+
+        let expresionFecha = new RegExp("^([0-9]{2})(/{1}|-{1})([0-9]{2})/{1}|-{1}([0-9]{4})$");
+
+        try {
+            let bisiesto = false;
+            let maximoDiasMes;
+            let [, dia,, mes, anno] = expresionFecha.exec(fecha);
+
+            // Paso los dias, meses y años a numeros enteros
+
+            dia = parseInt(dia);
+            mes = parseInt(mes);
+            anno = parseInt(fecha.slice(6,fecha.length));
+
+            // Compruebo que el año sea o no bisiesto
+
+            if ((anno % 4 == 0 && anno % 100 != 0) || anno % 400 == 0) {
+                bisiesto = true;
+            }
+            
+            // Asigno el numero de dias que tiene cada mes, teniendo en cuenta si es bisiesto ponerle
+            // Un dia mas a febrero
+
+            switch (mes) {
+                case 1, 3, 5, 7, 8, 10, 12:
+                    maximoDiasMes = 31;
+                    break;
+                case 4, 6, 9, 11:
+                    maximoDiasMes = 30;
+                    break;
+                default:
+                    if (!bisiesto) {
+                        maximoDiasMes = 28;
+                        break;
+                    }
+                    maximoDiasMes = 29;
+                    break;
+            }
+
+            if ((expresionFecha.test(fecha.trim()) && dia <= maximoDiasMes && mes <= 12 && anno <= 2020 && anno >1800)) {
+                return "";
+            }
+            else {
+                return "Fecha inválida";
+            }
+        } catch{
+            return "Fecha inválida";
+        }
+    }
+
+
+    let crearAlumno = () => {
+
+        if (document.getElementById("nombre").value &&
+            document.getElementById("apellido1").value &&
+            document.getElementById("apellido2").value &&
+            comprobarFechaNacimiento(document.getElementById("fecha").value) == "") {
+
+            // Creo la ventana con las preferencias asignadas
+
+            let ventanaUsuario = window.open("", "", "left=400px,top=300px,width=300px,height=200px");
+            ventanaUsuario.document.write(`
+                <html>
+                    <head>
+                        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+                        <title id="titulo">Javier Lopera Jiménez</title>
+                        <script src="js/Alumno.js"></script>
+                    </head>
+                    <body>
+                        <div id="container">
+                        
+                        </div>
+                    </body>
+                </html>
+                `
+            );
+            ventanaUsuario.document.close();
+        }
+        else{
+
+            for (const i of document.getElementsByTagName("fieldset")[0].getElementsByTagName("input")) {
+                if(i.value == ""){
+
+                    i.nextSibling.nextSibling.innerHTML = "Este campo no puede quedar vacío.";
+                    i.nextSibling.nextSibling.style.color = "red";
+
+                }
+                else{
+                    i.nextSibling.nextSibling.innerHTML = "";
+                }
+            }
+
+            if(comprobarFechaNacimiento(document.getElementById("fecha").value) != ""){
+                document.getElementById("msj_fecha").innerHTML = "Fecha incorrecta";
+                document.getElementById("msj_fecha").style.color = "red";
+            }
+            else{
+                document.getElementById("msj_fecha").innerHTML = "";
+            }
+        }
+    }
+
+    let init = function () {
+
+        document.getElementsByTagName("h1")[0].style.textAlign = "center";
+
+        document.getElementById("crear").addEventListener("click", crearAlumno);
+
+
+    }
+
+    document.addEventListener("DOMContentLoaded", init);
