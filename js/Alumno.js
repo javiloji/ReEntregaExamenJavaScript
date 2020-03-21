cd4cc36c8e6ccbc3f1d37b993480622701cd6ef9
javier <javierlopera988@gmail.com>
2020-03-21 01:34:58 +0100

    Añadiendo edad close #3 close #4

--- js/Alumno.js
+++ js/Alumno.js
@@ -8,21 +8,29 @@ function Alumno(nombre, apellido1, apellido2, fecha) {
     this.fecha = fecha;
 }
 
-let inicio = () => {
+let alumno;
+let fecha;
+
+document.addEventListener("DOMContentLoaded", function () {
     let nombre = opener.document.getElementById("nombre").value;
     let apellido1 = opener.document.getElementById("apellido1").value;
     let apellido2 = opener.document.getElementById("apellido2").value;
-    let fecha = opener.document.getElementById("fecha").value;
+     fecha = opener.document.getElementById("fecha").value;
 
     // Creo un nuevo usuario:
 
     alumno = new Alumno(nombre, apellido1, apellido2, fecha);
- 
+    
     alumno.mostrarAlumno();
-
-}
+});
 
  Alumno.prototype.mostrarAlumno = function (){
+
+    let fechaActual = new Date()
+
+    let fechaAlumno = new Date(alumno.fecha.substring(6,10) + "/" + alumno.fecha.substring(3,5) + "/" +  alumno.fecha.substring(0,2));
+
+    let resta = fechaActual.getTime() - fechaAlumno.getTime()
 
     container = document.getElementById("container");
     container.innerHTML = `
@@ -31,7 +39,10 @@ let inicio = () => {
                     <p>Apellido 1: `+ alumno.apellido1 + `</p>
                     <p>Apellido 2: `+ alumno.apellido2 + `</p>
                     <p>Fecha de Nacimiento: `+ alumno.fecha + `</p>
+                    <p>Edad: `+ Math.round(resta/ (1000*60*60*24*365)) + ` años</p>
+
+
         `;
 }
 
-document.addEventListener("DOMContentLoaded", inicio);
+
